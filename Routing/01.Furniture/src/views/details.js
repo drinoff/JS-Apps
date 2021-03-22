38d9e5a8ccb5ddedc560f4c688a3a34e146123ea
Drinoff <drinoff@gmail.com>
2021-03-22 19:29:29 +0100

    up

--- Routing/01.Furniture/src/views/details.js
+++ Routing/01.Furniture/src/views/details.js
@@ -1,7 +1,7 @@
-import {html} from '../../node_modules/lit-html/lit-html.js'
-import {getItemById} from '../api/data.js';
+import { html } from '../../node_modules/lit-html/lit-html.js'
+import { getItemById } from '../api/data.js';
 
-const itemDetailsTemplate = (data)=>html`
+const itemDetailsTemplate = (data) => html`
     <div class="container">
         <div class="row space-top">
             <div class="col-md-12">
@@ -25,11 +25,11 @@ const itemDetailsTemplate = (data)=>html`
                 <p>Material: <span>${data.material}</span></p>
                 <div>
                     ${(data._ownerId === sessionStorage.userId)
-                    ?
-                    html`<a href='/edit/${data._id}' class='btn btn-info'>Edit</a>
+        ?
+        html`<a href='/edit/${data._id}' class='btn btn-info'>Edit</a>
                     <a href='/' class="btn btn-red">Delete</a>`
-                    :''
-                    }
+        : ''
+    }
                 </div>
             </div>
         </div>
@@ -37,7 +37,12 @@ const itemDetailsTemplate = (data)=>html`
 `
 
 
-export async function detailsPage(ctx){
+export async function detailsPage(ctx) {
     const data = await getItemById(ctx.params.id);
     ctx.render(itemDetailsTemplate(data));
-    }
+    
+}
+
+
+
+
