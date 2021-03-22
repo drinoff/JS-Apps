208b48b395df828adcc39a0628c64199adee1e79
Drinoff <drinoff@gmail.com>
2021-03-22 08:41:17 +0100

    delete functionality and nav links to sort out for next time . Rest is Finished

--- Routing/01.Furniture/src/views/details.js
+++ Routing/01.Furniture/src/views/details.js
@@ -1,3 +1,43 @@
+import {html} from '../../node_modules/lit-html/lit-html.js'
+import {getItemById} from '../api/data.js';
+
+const itemDetailsTemplate = (data)=>html`
+    <div class="container">
+        <div class="row space-top">
+            <div class="col-md-12">
+                <h1>Furniture Details</h1>
+            </div>
+        </div>
+        <div class="row space-top">
+            <div class="col-md-4">
+                <div class="card text-white bg-primary">
+                    <div class="card-body">
+                        <img src=${`.${data.img}`} />
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <p>Make: <span>${data.make}</span></p>
+                <p>Model: <span>${data.model}</span></p>
+                <p>Year: <span>${data.year}</span></p>
+                <p>Description: <span>${data.description}</span></p>
+                <p>Price: <span>${data.price}</span></p>
+                <p>Material: <span>${data.material}</span></p>
+                <div>
+                    ${(data._ownerId === sessionStorage.userId)
+                    ?
+                    html`<a href='/edit/${data._id}' class='btn btn-info'>Edit</a>
+                    <a href='/' class="btn btn-red">Delete</a>`
+                    :''
+                    }
+                </div>
+            </div>
+        </div>
+    </div>
+`
+
+
 export async function detailsPage(ctx){
-    console.log('details page',ctx.params.id)
+    const data = await getItemById(ctx.params.id);
+    ctx.render(itemDetailsTemplate(data));
     }
--- Routing/01.Furniture/src/views/edit.js
+++ Routing/01.Furniture/src/views/edit.js
@@ -1,3 +1,83 @@
-export async function editPage(ctx){
-    console.log('edit page', ctx.params.id)
+import { html } from '../../node_modules/lit-html/lit-html.js'
+import { editItem, getItemById } from '../api/data.js'
+
+const editTemplate = (data, onSubmit) => html`
+<div class="container">
+    <div class="row space-top">
+        <div class="col-md-12">
+            <h1>Edit Furniture</h1>
+            <p>Please fill all fields.</p>
+        </div>
+    </div>
+    <form @submit=${onSubmit}>
+        <div class="row space-top">
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label class="form-control-label" for="new-make">Make</label>
+                    <input class="form-control" id="new-make" type="text" name="make" value=${data.make}>
+                </div>
+                <div class="form-group has-success">
+                    <label class="form-control-label" for="new-model">Model</label>
+                    <input class="form-control is-valid" id="new-model" type="text" name="model" value=${data.model}>
+                </div>
+                <div class="form-group has-danger">
+                    <label class="form-control-label" for="new-year">Year</label>
+                    <input class="form-control is-invalid" id="new-year" type="number" name="year" value=${data.year}>
+                </div>
+                <div class="form-group">
+                    <label class="form-control-label" for="new-description">Description</label>
+                    <input class="form-control" id="new-description" type="text" name='description'
+                        value=${data.description}>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label class="form-control-label" for="new-price">Price</label>
+                    <input class="form-control" id="new-price" type="number" name="price" value=${data.price}>
+                </div>
+                <div class="form-group">
+                    <label class="form-control-label" for="new-image">Image</label>
+                    <input class="form-control" id="new-image" type="text" name="img" value=${`.${data.img}`}>
+                </div>
+                <div class="form-group">
+                    <label class="form-control-label" for="new-material">Material (optional)</label>
+                    <input class="form-control" id="new-material" type="text" name="material" value=${data.material}>
+                </div>
+                <input type="submit" class="btn btn-info" value="Edit" />
+            </div>
+        </div>
+    </form>
+</div>
+`
+
+export async function editPage(ctx) {
+    const data = await getItemById(ctx.params.id);
+    ctx.render(editTemplate(data, onSubmit));
+
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        let formData = new FormData(event.target);
+
+        let make = formData.get('make');
+        let model = formData.get('model');
+        let year = formData.get('year');
+        let description = formData.get('description');
+        let price = formData.get('price');
+        let img = formData.get('img');
+        let material = formData.get('material');
+
+        let submitData = {
+            make,
+            model,
+            year,
+            description,
+            price,
+            img,
+            material
+        }
+        await editItem(ctx.params.id, submitData);
+        ctx.page.redirect('/')
+
     }
+};
